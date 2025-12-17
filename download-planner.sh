904f542a87e9ac2a203c36367207f841c634d291
Rahul <1rahulchandra1@gmail.com>
2025-12-17 12:56:56 +0100

    Refactor: update ChildSnack benchmark files and remove obsolete references

--- download-planner.sh
+++ download-planner.sh
@@ -3,7 +3,13 @@ set -e
 
 ./build.py
 
+# ./fast-downward.py \
+#   benchmarks/domains/AssemblyHierachial/domain.hddl \
+#   benchmarks/domains/AssemblyHierachial/30.hddl \
+#   --search "astar(lmcut())"
+
+
 ./fast-downward.py \
-  benchmarks/domains/AssemblyHierachial/domain.hddl \
-  benchmarks/domains/AssemblyHierachial/30.hddl \
+  benchmarks/domains/ChildSnack/domain.hddl \
+  benchmarks/domains/ChildSnack/01.hddl \
   --search "astar(lmcut())"
